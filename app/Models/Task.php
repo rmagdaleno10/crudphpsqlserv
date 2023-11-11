230f0f1bd924858c5eeb7ccf4f659b92ccf3049a
ramon-it <rmagdaleno@sanmatias.com>
2023-11-11 15:52:52 -0600

    Composer y Autoload PSR-4

---
+++ app/Models/Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CrudPhpSqlServ\App\Models;
+
+class Task
+{
+    public $title;
+    public $description;
+    public $status;
+
+    public function __construct() {
+
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Inicio";
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+use CrudPhpSqlServ\App\Models\Task;
+
+$obj = new Task();
+$obj->title = "Tarea 1";
+$obj->description = "Descripción 1";
+$obj->status = "Pendiente";
+
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
+//var_dump($_SERVER);
+
+
